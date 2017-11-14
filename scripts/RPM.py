18f70f2e2dac7cf0cb1a2dc29c8f6ee2201d321e
Patrick-Fenerty <fenerty64@gmail.com>
2017-11-14 15:55:26 -0600

    RPM

---
+++ scripts/RPM.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+from util.data import data_dir
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+rpm_url = 'http://www.espn.com/nba/statistics/rpm/_/page/{page}/sort/RPM'
+
+headers = []
+data = []
+for p in range(1, 12):
+    r = requests.post(rpm_url.format(page=p))
+    html = r.content
+    soup = BeautifulSoup(html)
+    if p == 1:
+        headers = [th.getText() for th in soup.find_all('tr')[0].find_all('td')]
+    rows = soup.find_all('tr')[1:]
+    data.extend([[td.getText() for td in rows[i].find_all('td')] for i in range(len(rows))])
+
+df = pd.DataFrame(data, columns=headers)
+df.RPM = df.RPM.astype(float)
+
+# df.to_csv(data_dir + 'RPM.csv')
+
+# traces = []
+# for t in df.TEAM.unique():
+#     team_df = df[df.TEAM == t]
+#     traces.append(go.Scatter(
+#         x=team_df.TEAM,
+#         y=team_df.RPM,
+#         mode='markers'
+#     ))
+#
+# traces.sort(key=lambda x: -x.y.mean())
+# py.plot(traces, filename='RPM')
