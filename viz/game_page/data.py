f06970192e1cf76631cdcd2bee939158d57c1375
Patrick-Fenerty <Patrick.Fenerty@geocent.com>
2018-02-15 15:11:22 -0600

    update to game page

--- viz/game_page/data.py
+++ viz/game_page/data.py
@@ -35,6 +35,7 @@ def get_scoring_stats_from_pbp(pbp_df):
         ft1a = len(ft_df[ft_df[desc].str.contains('of 1')])
         ft2a = len(ft_df[ft_df[desc].str.contains('of 2')])
         ft3a = len(ft_df[ft_df[desc].str.contains('of 3')])
+        ft_tech = len(ft_df[ft_df[desc].str.contains('Free Throw Technical')])
         ftm = len(ft_df[ft_df[desc].str.contains('PTS')])
 
         fg2_df = p_df[p_df['SHOT_TYPE'] == '2PT Field Goal']
@@ -54,6 +55,7 @@ def get_scoring_stats_from_pbp(pbp_df):
             'and_one': ft1a,
             '2pt_ft': ft2a,
             '3pt_ft': ft3a,
+            'tech_ft': ft_tech,
             'ftm': ftm,
             '2pt_fga': fg2a,
             '2pt_fgm': fg2m,
@@ -137,8 +139,8 @@ def get_stats_for_game(game_id, year, game_date, file_path, data_override=False)
 
 def get_data_for_game(game_id, game_date, year='2017-18'):
     get_rotation_data_for_game(game_id, year=year, single_game_file_path=file_dir)
-    # get_shot_data_for_game(game_id, season=year, file_path=file_dir + 'shots.json', data_override=False)
+    get_shot_data_for_game(game_id, season=year, file_path=file_dir + 'shots.json', data_override=False)
     # get_stats_for_game(game_id, year, game_date, file_dir + 'stats.json', data_override=True)
 
 
-get_data_for_game('0021700807', '02/07/2018')
+get_data_for_game('0021700862', '02/15/2018')
--- viz/game_page/js/rotations.js
+++ viz/game_page/js/rotations.js
@@ -1,13 +1,14 @@
 function plot_rotations(){
-  var margin = { top: 15, right: 150, bottom: 0, left: 150 },
+  var margin = { top: 15, right: 150, bottom: 0, left: 170 },
       width = 1200 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom,
       gridSize = Math.floor(width / 47),
       legendElementWidth = gridSize*2,
       buckets = 9,
       playerColors = colorbrewer.Greys[9],
-      minutes = ["Q1","","","","","","","","","","","","Q2","","","","","","","","","","","","Q3","","","","","","","","","","","","Q4","","","","","","","","","","","","OT1", "", "", "", "end"],
-      players = ["LeBron James","JR Smith","Isaiah Thomas","Jeff Green","Tristan Thompson","Jae Crowder","Cedi Osman","Channing Frye","Kyle Korver","Derrick Rose","", "Jimmy Butler","Andrew Wiggins","Jeff Teague","Karl-Anthony Towns","Taj Gibson","Jamal Crawford","Nemanja Bjelica","Gorgui Dieng","Tyus Jones"];
+      max_minute = 0,
+      top_team_player_count = 0,
+      max_pindex = 0;
 
   var svg = d3.select("#chart").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -15,48 +16,94 @@ function plot_rotations(){
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var dayLabels = svg.selectAll(".dayLabel")
-      .data(players)
-      .enter().append("text")
-        .text(function (d) { return d; })
-        .attr("x", 0)
-        .attr("y", function (d, i) { return (i * gridSize); })
-        .style("text-anchor", "end")
-        .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-        .attr("class", "dayLabel axis axis-workweek");
-
-  var timeLabels = svg.selectAll(".timeLabel")
-      .data(minutes)
-      .enter().append("text")
-        .text(function(d) { return d; })
-        .attr("x", function(d, i) { return (i * gridSize) -5; })
-        .attr("y", 210)
-        .style("text-anchor", "middle")
-        .attr("transform", "translate(" + (gridSize - 13) * 2 + ", -6)")
-        .attr("class", "timeLabel mono axis axis-worktime");
-
   var heatmapChart = function(csvFile) {
     d3.csv(csvFile,
     function(d) {
       return {
-        day: +d.player,
-        hour: +d.minute,
+        player: d.player,
+        minute: +d.minute,
         value: +d.value,
         pindex: +d.pindex
       };
     },
     function(error, data) {
+
+      data.sort(function(a,b) {
+        if (a.pindex == b.pindex)
+          return 0;
+        if (a.pindex < b.pindex)
+          return -1;
+        if (a.pindex > b.pindex)
+          return 1;
+      });
+
+      var players = [],
+          previous_pindex = 0;
+
+      $.each(data, function(i, d) {
+        if ($.inArray(this.player, players) === -1) {
+          if (this.pindex - previous_pindex > 1) {
+            players.push("");
+            top_team_player_count=this.pindex-1;
+          }
+          players.push(this.player);
+        }
+        previous_pindex = this.pindex;
+
+        if(this.pindex > max_pindex) {
+          max_pindex = this.pindex;
+        }
+
+        if(this.minute > max_minute) {
+          max_minute = this.minute;
+        }
+      });
+
+      var playerLabels = svg.selectAll(".playerLabel")
+          .data(players)
+          .enter().append("text")
+            .text(function (d) { return d; })
+            .attr("x", 0)
+            .attr("y", function (d, i) { return (i * gridSize); })
+            .style("text-anchor", "end")
+            .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+            .attr("class", "dayLabel axis axis-workweek");
+
+      minutes = ["Q1","","","","","","","","","","","","Q2","","","","","","","","","","","","Q3","","","","","","","","","","","","Q4","","","","","","","","","",""];
+      if (max_minute >= 48) {
+        extra_minutes = max_minute - 48;
+        for (i = 0; i < extra_minutes; i++) {
+          if(i % 5 == 0) {
+            minutes.push("OT" + ((i + 1) - (5 * i)));
+          }
+          else {
+            minutes.push("");
+          }
+        };
+      }
+      minutes.push('end');
+
+      var timeLabels = svg.selectAll(".timeLabel")
+          .data(minutes)
+          .enter().append("text")
+            .text(function(d) { return d; })
+            .attr("x", function(d, i) { return (i * gridSize) -5; })
+            .attr("y", top_team_player_count * gridSize)
+            .style("text-anchor", "middle")
+            .attr("transform", "translate(" + (gridSize - 13) * 2 + ", -6)")
+            .attr("class", "timeLabel mono axis axis-worktime");
+
       var playerColorScale = d3.scaleQuantile()
           .domain([0, 60])
           .range(playerColors);
 
       var cards = svg.selectAll(".hour")
-          .data(data, function(d) {return d.pindex+':'+d.hour;});
+          .data(data, function(d) {return d.pindex+':'+d.minute;});
 
       cards.append("title");
 
       cards.enter().append("rect")
-          .attr("x", function(d) { return (d.hour - 1) * gridSize; })
+          .attr("x", function(d) { return (d.minute - 1) * gridSize; })
           .attr("y", function(d) { return (d.pindex - 1) * gridSize; })
           .attr("rx", 4)
           .attr("ry", 4)
@@ -71,20 +118,6 @@ function plot_rotations(){
     });
   };
 
-  var yAxisShift = 170;
-  var xAxisShift = 260;
-
-  var x = d3.scaleLinear()
-    .rangeRound([margin.left, width + xAxisShift]);
-
-  var y = d3.scaleLinear()
-    .rangeRound([height - margin.top, margin.bottom + yAxisShift]);
-
-  var line = d3.line()
-    .x(function(d) { return x(d.minute); })
-    .y(function(d) { return y(d.score_margin); })
-    .curve(d3.curveStep);
-
   var lineChart = function(csvFile) {
     d3.csv(csvFile,
       function(d) {
@@ -93,12 +126,47 @@ function plot_rotations(){
           score_margin: +d.score_margin
         };
       }, function(error, data){
+        var max_home_lead = 0,
+            max_vis_lead = 0;
+
+        $.each(data, function(i, d) {
+            if (this.score_margin > max_home_lead) {
+              max_home_lead = this.score_margin;
+            }
+            if (this.score_margin < max_vis_lead) {
+              max_vis_lead = this.score_margin;
+            }
+        });
+        max_vis_lead = Math.abs(max_vis_lead);
+
+        var max_lead_diff = max_home_lead - max_vis_lead,
+            max_lead = Math.max(max_home_lead, max_vis_lead),
+            max_lead_ratio = Math.min(max_home_lead, max_vis_lead) / Math.max(max_home_lead, max_vis_lead);
+
+        var yAxisShiftBool = max_lead_ratio == max_home_lead ? true : false;
+
+        var yAxisScale = height - (((max_pindex * gridSize) / 2) * (1 + max_lead_ratio)),
+            yAxisShift = yAxisShiftBool ? (1 - max_lead_ratio) * (height / 2) : 0;
+
+        console.log(yAxisShift);
+
+        var x = d3.scaleLinear()
+          .rangeRound([margin.left, width + margin.right]);
+
+        var y = d3.scaleLinear()
+          .rangeRound([height - margin.top, yAxisScale]);
+
+        var line = d3.line()
+          .x(function(d) { return x(d.minute); })
+          .y(function(d) { return y(d.score_margin); })
+          .curve(d3.curveStep);
+
         x.domain(d3.extent(data, function(d) { return +d.minute; }));
         y.domain(d3.extent(data, function(d) { return +d.score_margin; }));
 
         svg.append("g")
           .call(d3.axisRight(y))
-          .attr("transform", "translate(1010, " + (margin.bottom - yAxisShift) + ")")
+          .attr("transform", "translate(" + width + ", " + (margin.bottom - yAxisScale + yAxisShift) + ")")
           .append("text")
           .attr("fill", "#000")
           .attr("transform", "rotate(-90)")
@@ -115,7 +183,7 @@ function plot_rotations(){
           .attr("stroke-linecap", "square")
           .attr("stroke-width", 3.5)
           .attr("d", line)
-          .attr("transform", "translate(" + -150 + "," + (margin.bottom - yAxisShift) + ")");;
+          .attr("transform", "translate(" + -margin.left + "," + (margin.bottom - yAxisScale + yAxisShift) + ")");;
       })
   };
   heatmapChart("./data/data.csv");
