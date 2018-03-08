45985d0405db0596a32e26ad264e694102951959
Patrick-Fenerty <fenerty64@gmail.com>
2018-03-07 20:32:31 -0600

    Change to Pelicans Schedule Script
    Using new buckets

--- viz/PelicansSchedule/data.py
+++ viz/PelicansSchedule/data.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import datetime
 from util.nba_stats import Standings
 
 # Import Data
 schedule = pd.read_csv('./schedule.csv')
-remaining_schedule = schedule[schedule['Home_Pts'].isnull()]
+
+current_date = datetime.date.today()
+schedule['Date'] = schedule['Date'].map(lambda x:
+                                        x.split(' ')[1] + '-' +
+                                        (x.split(' ')[2] if len(x.split(' ')[2]) == 2 else '0' + x.split(' ')[2]) +
+                                        '-' + x.split(' ')[3])
+
+schedule['Date'] = pd.to_datetime(schedule['Date'], infer_datetime_format=True)
+remaining_schedule = schedule[schedule['Date'] >= current_date]
 
 standings = Standings().get_data({})
 standings['Team'] = standings['TeamCity'].map(str) + ' ' + standings['TeamName'].map(str)
@@ -12,61 +21,30 @@ standings = standings[['Team', 'Conference', 'WINS', 'LOSSES', 'WinPCT', 'Playof
 # Standings uses LA Clippers / Schedule uses Los Angeles Clippers: make consistent
 standings.set_value(16, 'Team', 'Los Angeles Clippers')
 
-# Determine win% needed to finish .500
-standings['500'] = (41 - standings['WINS']) / (82 - standings['WINS'] - standings['LOSSES'])
-
-# Define team categories
-team_categories = {
-    'Pelicans': standings['Team'] == 'New Orleans Pelicans',
-    'Top': (standings['500'] <= 1 / 3) & (standings['Conference'] == 'West'),
-    'HopeIn': (standings['500'] > 1 / 3) & (standings['500'] <= 2 / 3) & (standings['PlayoffRank'] <= 8) & (
-                standings['Conference'] == 'West'),
-    'HopeOut': (standings['500'] > 1 / 3) & (standings['500'] <= 2 / 3) & (standings['PlayoffRank'] > 8) & (
-                standings['Conference'] == 'West'),
-    'Bottom': (standings['500'] > 2 / 3) & (standings['Conference'] == 'West'),
-    'East': standings['Conference'] == 'East'
-}
-
-# Set Category Column according to defined conditions
-standings['Category'] = ''
-for c in team_categories:
-    standings.loc[team_categories[c], 'Category'] = c
+teams = standings[
+    (standings['Conference'] == 'West') &
+    (standings['PlayoffRank'] >= 3) &
+    (standings['PlayoffRank'] <= 10)]['Team'].tolist()
 
 
-# Convert standings dataframe to dictionary for easier lookup
-standings_dict = standings.set_index('Team').to_dict('index')
+data = []
+for team1 in teams:
+    team_df = remaining_schedule[
+        (remaining_schedule['Home'] == team1) | (remaining_schedule['Visitor'] == team1)
+    ]
 
-# Create dict of game categories from team categories
-game_categories = {}
-for tc1 in team_categories:
-    for tc2 in team_categories:
-        if (tc2 + ' vs ' + tc1) not in game_categories:
-            game_categories[tc1 + ' vs ' + tc2] = 0
+    team_data = {'Team': team1}
 
-# Iterate through schedule, assign each game a category, and add to categories count
-schedule_category_column = []
-for ix, game in remaining_schedule.iterrows():
-    home_category = standings_dict[game['Home']]['Category']
-    visitor_category = standings_dict[game['Visitor']]['Category']
+    for team2 in teams:
+        if team1 != team2:
+            matchup_df = team_df[
+                (team_df['Home'] == team2) | (team_df['Visitor'] == team2)
+            ]
+            team_data[team2] = len(matchup_df)
+        else:
+            team_data[team2] = 0
 
-    game_category = home_category + ' vs ' + visitor_category
-    game_category = game_category if game_category in game_categories else visitor_category + ' vs ' + home_category
+    data.append(team_data)
 
-    game_categories[game_category] += 1
-    schedule_category_column.append(game_category)
-
-remaining_schedule['Category'] = pd.Series(schedule_category_column)
-
-# Create matrix of game counts by team categories
-df = pd.DataFrame(columns=['OPP'] + list(team_categories.keys()))
-df['OPP'] = list(team_categories.keys())
-df = df.fillna(0)
-
-for gc in game_categories:
-    gc1, gc2 = gc.split(' vs ')
-    df.loc[df['OPP'] == gc1, gc2] += game_categories[gc]
-    df.loc[df['OPP'] == gc2, gc1] += game_categories[gc]
-
-df.to_csv('game_counts.csv')
-remaining_schedule.to_csv('categorized_schedule.csv')
+head_to_head_df = pd.DataFrame(data)[['Team'] + teams]
 
