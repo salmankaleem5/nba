ce1b083ea7944e5171391304788eac10f43060f9
Unknown <patrick-fenerty@protonmail.com>
2018-03-07 20:04:50 -0600

    Changes for python3.5
    Removed f-string instances
    Moved data directory outside of repo directory

--- util/data.py
+++ util/data.py
@@ -1,27 +1,19 @@
 import os
 import platform
 
-
-wd = os.getcwd()
-
-if 'Windows' in platform.platform():
-    split = '\\'
-
+if "Windows" in platform.platform():
+    data_dir = 'C:/data/nba'
+elif "Darwin" in platform.platform():
+    data_dir = '/Users/patrick/data/nba/'
 else:
-    split = '/'
-
-nba_index = wd.split(split).index('nba')
-
-data_dir = '/'.join(
-    wd.split(split)[:nba_index + 1] + ['data/']
-)
+    data_dir = '/home/patrick/data/nba/'
 
 
 def file_check(file_path):
     split_path = file_path.split('/')
-    current_path = '/'.join(split_path[:-1])
-
-    if not os.path.exists(current_path):
-        os.makedirs(current_path)
-
+    current_path = ''
+    for p in range(0, len(split_path) - 1):
+        current_path += split_path[p] + '/'
+        if not os.path.exists(current_path):
+            os.makedirs(current_path)
     return os.path.isfile(file_path)
