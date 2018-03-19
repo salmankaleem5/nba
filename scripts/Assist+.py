6b04f62c0a0592e3e1a2ba7ec3b425aedc416091
Unknown <Patrick.Fenerty@geocent.com>
2018-03-19 13:52:17 -0500

    Updates to Assist+ Script

--- scripts/Assist+.py
+++ scripts/Assist+.py
@@ -6,11 +6,9 @@ import pandas as pd
 
 general_player_ep = GeneralPlayerStats()
 tracking_ep = TrackingStats()
-data_override = False
-season_range = (2017, 2017)
 
 
-def get_stats_for_player_season(season, games_filter=20, assist_filter=4, override_file=False):
+def get_stats_for_player_season(season, games_filter=50, assist_filter=5, override_file=False):
     shots_df = merge_shot_pbp_for_season(season, override_file=override_file)
 
     # Calculate average efficiency from each zone of the court
@@ -37,8 +35,11 @@ def get_stats_for_player_season(season, games_filter=20, assist_filter=4, overri
         'MeasureType': 'Base'
     }, override_file=override_file)
 
-    general_stats_df = general_stats_df[general_stats_df.GP >= games_filter]
-    players = general_stats_df[general_stats_df.AST > assist_filter].PLAYER_NAME.tolist()
+    general_stats_df = general_stats_df[
+        (general_stats_df.GP >= games_filter) &
+        (general_stats_df.AST > assist_filter)
+    ]
+    players = general_stats_df.PLAYER_NAME.tolist()
 
     # For each player filter shots for ones they assisted and calculate what percentage comes from each zone
     # Use those percentages with average efficiencies to calculate expected points produced per potential assist
@@ -50,17 +51,18 @@ def get_stats_for_player_season(season, games_filter=20, assist_filter=4, overri
             'YEAR': season,
             'AST': len(player_assist_df),
             'EXP_PTS_PER_PAST': 0,
-            'GP': len(player_assist_df.GAME_ID.unique())
+            'GP': general_stats_df[general_stats_df['PLAYER_NAME'] == p].iloc[0]['GP']
         }
 
-        for z in shot_zones:
-            player_zone_df = player_assist_df[player_assist_df['SHOT_ZONE_BASIC'].str.contains(z)]
-            zone_assists = len(player_zone_df)
-            zone_pct = round(zone_assists / player_data['AST'], 2)
-            player_data[z] = zone_pct
-            player_data['EXP_PTS_PER_PAST'] += zone_pct * shot_zones[z]
+        if player_data['AST'] > 0:
+            for z in shot_zones:
+                player_zone_df = player_assist_df[player_assist_df['SHOT_ZONE_BASIC'].str.contains(z)]
+                zone_assists = len(player_zone_df)
+                zone_pct = round(zone_assists / player_data['AST'], 4)
+                player_data[z] = zone_pct * 100
+                player_data['EXP_PTS_PER_PAST'] += zone_pct * shot_zones[z]
 
-        assist_data.append(player_data)
+            assist_data.append(player_data)
 
     assist_df = pd.DataFrame(assist_data)
 
@@ -77,22 +79,22 @@ def get_stats_for_player_season(season, games_filter=20, assist_filter=4, overri
         'In The Paint \(Non-RA\)': 'PAINT_NON-RA'
     })
 
-    assist_df['AST_PTS'] = (assist_df['AST'] * assist_df['PCT_3'] * 3) + (
-        (assist_df['AST'] * (1 - assist_df['PCT_3']) * 2))
+    assist_df['AST_PTS'] = round((assist_df['AST'] * assist_df['PCT_3'] / 100 * 3) + (
+        (assist_df['AST'] * (1 - assist_df['PCT_3'] / 100) * 2)), 0)
 
     # Add tracking data to determine actual points produced per potential assist
-    tracking_df = tracking_ep.get_data({
-        'Season': season,
-        'PerMode': 'Totals',
-        'PtMeasureType': 'Passing'
-    }, override_file=override_file)[['PLAYER_NAME', 'POTENTIAL_AST']]
-
-    assist_df = assist_df.merge(tracking_df, on='PLAYER_NAME', how='inner')
-
-    assist_df['PTS_PER_PAST'] = assist_df['AST_PTS'] / assist_df['POTENTIAL_AST']
-    assist_df['PTS_PER_PAST_DIFF'] = assist_df['PTS_PER_PAST'] - assist_df['EXP_PTS_PER_PAST']
-
-    assist_df = assist_df.sort_values(by='PTS_PER_PAST', ascending=False)
+    # tracking_df = tracking_ep.get_data({
+    #     'Season': season,
+    #     'PerMode': 'Totals',
+    #     'PtMeasureType': 'Passing'
+    # }, override_file=override_file)[['PLAYER_NAME', 'POTENTIAL_AST']]
+    #
+    # assist_df = assist_df.merge(tracking_df, on='PLAYER_NAME', how='inner')
+    #
+    # assist_df['PTS_PER_PAST'] = assist_df['AST_PTS'] / assist_df['POTENTIAL_AST']
+    # assist_df['PTS_PER_PAST_DIFF'] = assist_df['PTS_PER_PAST'] - assist_df['EXP_PTS_PER_PAST']
+    #
+    # assist_df = assist_df.sort_values(by='PTS_PER_PAST', ascending=False)
 
     return assist_df
 
@@ -101,8 +103,14 @@ def get_stats_for_player_year_range(year_range):
     assist_df = pd.DataFrame()
     for year in year_range:
         season = get_year_string(year)
-        assist_df = assist_df.append(get_stats_for_player_season(season))
-        assist_df = assist_df.sort_values(by='PTS_PER_PAST', ascending=False)
+        assist_df = assist_df.append(
+            get_stats_for_player_season(
+                season,
+                assist_filter=4,
+                override_file=False
+            )
+        )
+        assist_df = assist_df.sort_values(by='EXP_PTS_PER_PAST', ascending=False)
     return assist_df
 
 
@@ -209,5 +217,11 @@ def get_stats_for_team_year_range(year_range):
     return assist_df
 
 
-xdf = get_stats_for_player_season('2017-18', override_file=True)
+# xdf = get_stats_for_player_season('2017-18', override_file=True)
+xdf = get_stats_for_player_year_range(range(1996, 2018))
+xdf.to_csv('assist.csv')
+xdf['APG'] = xdf['AST'] / xdf['GP']
+xdf['PTS_PER_AST'] = xdf['AST_PTS'] / xdf['AST']
+xdf = xdf.sort_values(by='RESTRICTED_AREA', ascending=False).head(50)
+print_reddit_table(xdf, ['PLAYER_NAME', 'YEAR', 'GP', 'AST', 'APG', 'RESTRICTED_AREA', 'PCT_MID', 'PCT_3', 'PTS_PER_AST', 'EXP_PTS_PER_PAST'])
 None
