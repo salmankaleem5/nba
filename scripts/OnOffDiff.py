eeb29eee0d7a4ab5eba43ed7543129f7aedd2a6c
Patrick-Fenerty <Patrick.Fenerty@geocent.com>
2018-03-19 09:02:10 -0500

    Updates to on off

--- scripts/OnOffDiff.py
+++ scripts/OnOffDiff.py
@@ -1,36 +1,33 @@
 from util.nba_stats import OnOffSummary, GeneralTeamStats
 from util.reddit import print_reddit_table
+from util.format import get_year_string
 import pandas as pd
 
 onOffStats = OnOffSummary()
 teamStats = GeneralTeamStats()
 
-teams = teamStats.get_data({'Season': '2017-18'}).TEAM_ID.unique()
 
-onOffDf = pd.DataFrame()
-for t in teams:
-    onOffDf = onOffDf.append(onOffStats.get_data({'TeamID': t}, override_file=False))
+def get_on_off_for_year(season):
+    teams = teamStats.get_data({'Season': season}).TEAM_ID.unique()
 
-onOffDf['OFF_DIFF'] = onOffDf['OFF_RATING_ON'] - onOffDf['OFF_RATING_OFF']
-onOffDf['DEF_DIFF'] = onOffDf['DEF_RATING_OFF'] - onOffDf['DEF_RATING_ON']
-onOffDf['NET_DIFF'] = onOffDf['OFF_DIFF'] + onOffDf['DEF_DIFF']
+    on_off_df = pd.DataFrame()
+    for t in teams:
+        on_off_df = on_off_df.append(onOffStats.get_data({'TeamID': t, 'Season': season}, override_file=False))
 
+    on_off_df = on_off_df[on_off_df.MIN_ON >= 1000]
 
-onOffDf = onOffDf[onOffDf.MIN_ON >= 500].sort_values(by='NET_DIFF', ascending=False)
+    on_off_df['OFF_DIFF'] = on_off_df['OFF_RATING_ON'] - on_off_df['OFF_RATING_OFF']
+    on_off_df['DEF_DIFF'] = on_off_df['DEF_RATING_OFF'] - on_off_df['DEF_RATING_ON']
+    on_off_df['NET_DIFF'] = on_off_df['OFF_DIFF'] + on_off_df['DEF_DIFF']
 
-df = pd.DataFrame(columns=onOffDf.columns)
-for t in onOffDf.TEAM_ABBREVIATION.unique():
-    df = df.append(onOffDf[onOffDf['TEAM_ABBREVIATION'] == t].iloc[0])
-
-print_reddit_table(df,
-                   ['VS_PLAYER_NAME', 'TEAM_ABBREVIATION', 'NET_DIFF'])
+    on_off_df['YEAR'] = season
+    return on_off_df
 
 
-onOffDf = onOffDf[onOffDf.MIN_ON >= 500].sort_values(by='NET_DIFF', ascending=True)
+df = pd.DataFrame()
+for year in range(2007, 2018):
+    year_string = get_year_string(year)
+    df = df.append(get_on_off_for_year(year_string))
 
-df = pd.DataFrame(columns=onOffDf.columns)
-for t in onOffDf.TEAM_ABBREVIATION.unique():
-    df = df.append(onOffDf[onOffDf['TEAM_ABBREVIATION'] == t].iloc[0])
-
-print_reddit_table(df,
-                   ['VS_PLAYER_NAME', 'TEAM_ABBREVIATION', 'NET_DIFF'])
+df = df.sort_values(by='NET_DIFF', ascending=False)
+print_reddit_table(df, ['VS_PLAYER_NAME', 'YEAR', 'TEAM_ABBREVIATION', 'NET_DIFF'])
